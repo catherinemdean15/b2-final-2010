4266c22767a8d1b3a8f6c07e1480a22f1203e7e6
Catherine Dean <catherinemdean15@gmail.com>
2021-01-19 11:29:34 -0700

    create class method average_player_age

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,7 @@ class Team < ApplicationRecord
   has_many :competitions, through: :competitions_teams
   has_many :players
 
+  def self.average_player_age
+    Team.joins(:players).select('teams.*, avg(age) as avg_age').group('teams.id').order('avg_age desc')
+  end
 end
