e6db3e97dcaf5e402c4a7fff674a2768a64c9480
Catherine Dean <catherinemdean15@gmail.com>
2021-01-19 09:57:29 -0700

    create average_age method

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,4 +2,7 @@ class Competition < ApplicationRecord
   has_many :competitions_teams
   has_many :teams, through: :competitions_teams
 
+  def average_age
+    teams.joins(:players).average(:age).to_f
+  end
 end
