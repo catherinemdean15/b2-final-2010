3664dc0d817456a85590e47aae1684ca6e60ef62
Catherine Dean <catherinemdean15@gmail.com>
2021-01-19 10:30:17 -0700

    add create action, before action, and params permit action to teams_controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,23 @@
 class TeamsController < ApplicationController
+  before_action :find_competition
+
   def new
-    
+
+  end
+
+  def create
+    @competition.teams.create!(teams_params)
+    redirect_to competition_path(@competition)
+  end
+
+  private
+
+  def teams_params
+    params.require(:competition).permit(:hometown, :nickname)
+  end
+
+  def find_competition
+    @competition = Competition.find(params[:competition_id])
   end
 
 end
